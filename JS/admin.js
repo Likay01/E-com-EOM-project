bccf72d06143582ca0fcaf23e43d56e3827d069b
Likay01 <mgoqilindokuhle2gmail.com>
2022-12-09 16:23:29 +0200

    update

--- JS/admin.js
+++ JS/admin.js
@@ -1,99 +1,104 @@
-let admin = JSON.parse(localStorage.getItem('content')) ?
-JSON.parse(localStorage.getItem('content')) : [
+let Vinyl = JSON.parse(localStorage.getItem('Albums')) ?
+JSON.parse(localStorage.getItem('Albums')) : [
     {
         id: 1,
         VinylImage:"https://i.postimg.cc/d13G5Bnp/1999-by-Joey-Bada.jpg",
         VinylsTitle:"1999",
         VInylName:"Joey Bada$$",
-        VinylPrice: 180,
+        VinylPrice: 380,
     },
     {
         id: 2,
         VinylImage:"https://i.postimg.cc/v8r9fsCg/Graduation-by-Kanye-West.jpg",
         VinylsTitle:"Graduation",
         VInylName:"Kanye West",
-        VinylPrice: 200,
+        VinylPrice: 400,
     },
     {
         id: 3,
         VinylImage:"https://i.postimg.cc/wMCrssvr/Kendrick-Lamar-s-To-Pimp-A-Butterfly-is-finally-getting-a-vinyl-release-The-Vinyl-Factory.jpg",
         VinylsTitle:"To pimp a butterfly",
         VInylName:"Kendrick lamar",
-        VinylPrice: 200,
+        VinylPrice: 400,
     },
     {
         id: 4,
         VinylImage:"https://i.postimg.cc/W3cFqq4F/Sonder-Son-by-Brent-Faiyaz.jpg",
         VinylsTitle:"Sonder Son",
         VInylName:"Brent Faiyaz",
-        VinylPrice: 180,
+        VinylPrice: 380,
     },
     {
         id: 5,
         VinylImage:"https://i.postimg.cc/05w6dfS6/More-Life.jpg",
         VinylsTitle:"More life",
         VInylName:"Drake",
-        VinylPrice: 220,
+        VinylPrice: 380,
     },
     {
         id: 6,
         VinylImage:"https://i.postimg.cc/pXJ9hdNy/PRBLMS-6-LACK.jpg",
         VinylsTitle:"PRBLMS",
         VInylName:"6lack",
-        VinylPrice: 170,
+        VinylPrice: 290,
     },
     {
         id: 7,
         VinylImage:"https://i.postimg.cc/pTmGr7pX/J-Cole-2014-Forest-Hills-Drive.jpg",
         VinylsTitle:"2014 Forest Hills Drive",
         VInylName:"J cole",
-        VinylPrice: 230,
+        VinylPrice: 420,
     },
     {
         id: 8,
         VinylImage:"https://i.postimg.cc/yx6MLnNM/100-Album-Covers-From-2016-That-Will-Leave-You-Speechless.jpg",
         VinylsTitle:"Coloring book",
         VInylName:"Chance the rapper",
-        VinylPrice: 170,
+        VinylPrice: 300,
     },
 ];
 function displayAdjustments(){
-    Object.keys(admin).forEach((album)=> {
-        document.querySelector('.content').innerHTML +=
+    Object.keys(Vinyl).forEach((album)=> {
+        document.querySelector('.Albums').innerHTML +=
         `<tr>
-        <th scope="row">${admin[album].id}</th>
-        <td><img class="addpic" src="${admin[album].VinylImage}" class="card-img-top" alt="..."></td>
-        <td>${admin[album].VinylsTitle} <br>${admin[album].VInylName}</br></td>
-        <td>R ${admin[album].VinylPrice}</td>
-        <td><button type="button" class="butt btn-light">Edit</button><button type="button" onclick = "Erase()" class="del btn-light">Delete</button></td>
-        <td></td>
+        <th scope="row">${Vinyl[album].id}</th>
+        <td><img class="addpic" src="${Vinyl[album].VinylImage}" class="card-img-top" alt="..."></td>
+        <td>${Vinyl[album].VinylsTitle} <br>${Vinyl[album].VInylName}</br></td>
+        <td>R ${Vinyl[album].VinylPrice}</td>
+        <td><button type="button" class="butt btn-light">Edit</button><button type="button" onclick = "Erase(${Vinyl[album].id})" class="del btn-light">Delete</button></td>
+        
       </tr>`
 })
 }
 displayAdjustments();
-localStorage.setItem('content', JSON.stringify(admin));
+localStorage.setItem('Albums', JSON.stringify(Vinyl));
 
 // delete button 
 function Erase(id) {
-localStorage.setItem('content', JSON.stringify(admin));
+localStorage.setItem('Albums', JSON.stringify(Vinyl));
 document.querySelector('#del');
-admin.splice(id-1, 1)
-localStorage.setItem('content', JSON.stringify(admin))
+Vinyl.splice(id-1, 1)
+localStorage.setItem('Albums', JSON.stringify(Vinyl))
+location.reload()
 }
 
 
 
 // Add button 
-// add event listener 
-admin.addEventListener('click',(e)=>{
+let confirm= document.querySelector('.confirm') 
+console.log(confirm.outerHTML)
+confirm.addEventListener('click',(e)=>{
 e.preventDefault();
-let title = document.querySelector('#VinylsTitle').value;
-let artist = document.querySelector('#VInylName').value;
-let price = document.querySelector('#VinylPrice').value;
+
+let id = document.querySelector('#Vinylid').value;
+let VinylImage = document.querySelector('#VinylCover').value;
+let VinylsTitle = document.querySelector('#VinylTitle').value;
+let VInylName = document.querySelector('#Artists').value;
+let VinylPrice = document.querySelector('#Pricing').value;
 
 
 // Push an Object into an array 
-admin.push(
+Vinyl.push(
     {
         id,
         VinylImage,
@@ -102,6 +107,10 @@ admin.push(
         VinylPrice
     }
 )
-localStorage.setItem('content', JSON.stringify(admin));
-})
-;
+localStorage.setItem('Albums', JSON.stringify(Vinyl));
+console.table(JSON.parse(localStorage.Albums))
+location.reload()
+});
+localStorage.setItem('Albums', JSON.stringify(Vinyl));
+
+// edit btn
